1e2b035cb0049445049cf8fd126db5e3bf07c0d2
xTavares <joeltavaresxz@gmail.com>
2026-06-07 18:54:32 +0100

    Update dragging.lua

--- src/modules/dragging.lua
+++ src/modules/dragging.lua
@@ -4,25 +4,21 @@ local Dragging = {}
 
 function Dragging:MakeDraggable(guiObject)
 	local dragging = false
-	local wasDragged = false
-	local dragInput = nil
-	local startMouse = nil
-	local startPosition = nil
+	local dragInput
+	local mouseStart
+	local frameStart
 
 	guiObject.Active = true
 
 	guiObject.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 			dragging = true
-			wasDragged = false
-			startMouse = input.Position
-			startPosition = guiObject.Position
+			mouseStart = input.Position
+			frameStart = guiObject.Position
 
 			input.Changed:Connect(function()
 				if input.UserInputState == Enum.UserInputState.End then
-					task.delay(0.05, function()
-						dragging = false
-					end)
+					dragging = false
 				end
 			end)
 		end
@@ -35,25 +31,17 @@ function Dragging:MakeDraggable(guiObject)
 	end)
 
 	UserInputService.InputChanged:Connect(function(input)
-		if dragging and input == dragInput and startMouse and startPosition then
-			local delta = input.Position - startMouse
-
-			if math.abs(delta.X) > 4 or math.abs(delta.Y) > 4 then
-				wasDragged = true
-			end
+		if dragging and input == dragInput then
+			local delta = input.Position - mouseStart
 
 			guiObject.Position = UDim2.new(
-				startPosition.X.Scale,
-				startPosition.X.Offset + delta.X,
-				startPosition.Y.Scale,
-				startPosition.Y.Offset + delta.Y
+				frameStart.X.Scale,
+				frameStart.X.Offset + delta.X,
+				frameStart.Y.Scale,
+				frameStart.Y.Offset + delta.Y
 			)
 		end
 	end)
-
-	return function()
-		return wasDragged
-	end
 end
 
 return Dragging
