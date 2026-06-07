b9d1852b416fa7f49ef29f3013fe1557207c6a51
xTavares <joeltavaresxz@gmail.com>
2026-06-07 17:04:16 +0100

    Implement tab controller with section switching

---
+++ src/modules/tabs.lua
@@ -0,0 +1,65 @@
+local Tabs = {}
+
+function Tabs:Create(sections, utils)
+	local controller = {}
+	controller.CurrentSection = nil
+
+	function controller:Switch(section)
+		if self.CurrentSection == section then
+			return
+		end
+
+		if self.CurrentSection then
+			self.CurrentSection.TabBtn.BackgroundTransparency = 1
+
+			utils:Tween(self.CurrentSection.Container, {
+				Position = UDim2.new(0.5, 0, 1, 0)
+			}, 0.18)
+		end
+
+		section.Container.Visible = true
+		section.TabBtn.BackgroundTransparency = 0
+
+		utils:Tween(section.Container, {
+			Position = UDim2.new(0.5, 0, 0, 0)
+		}, 0.18)
+
+		self.CurrentSection = section
+	end
+
+	function controller:Init(defaultSection)
+		for _, section in pairs(sections) do
+			section.TabBtn.MouseEnter:Connect(function()
+				local shadow = section.TabBtn:FindFirstChild("InnerShadow")
+
+				if shadow then
+					shadow.Transparency = 0.95
+				end
+			end)
+
+			section.TabBtn.MouseLeave:Connect(function()
+				local shadow = section.TabBtn:FindFirstChild("InnerShadow")
+
+				if shadow then
+					shadow.Transparency = 1
+				end
+			end)
+
+			section.TabBtn.MouseButton1Click:Connect(function()
+				self:Switch(section)
+			end)
+
+			section.TabBtn.BackgroundTransparency = 1
+			section.Container.Visible = false
+			section.Container.Position = UDim2.new(0.5, 0, 1, 0)
+		end
+
+		if defaultSection then
+			self:Switch(defaultSection)
+		end
+	end
+
+	return controller
+end
+
+return Tabs
