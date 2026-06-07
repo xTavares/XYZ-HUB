1c5f6ce296ee31ff920608f3c82bf19b014307ef
xTavares <joeltavaresxz@gmail.com>
2026-06-07 19:37:47 +0100

    Create places.lua

---
+++ src/data/places.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		Name = "Sail For Brainrots",
+		PlaceId = "89046742932569",
+		Module = "89046742932569.lua",
+		Status = "🟢",
+		Supported = true,
+		Downtime = {
+			Enabled = false,
+			Message = ""
+		},
+		Update = {
+			Version = "1.0.0",
+			Log = {
+				"Initial Support Added.",
+				"Premium Game Page Enabled."
+			}
+		}
+	}
+}
