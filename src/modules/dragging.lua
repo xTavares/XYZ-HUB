0a413c7e0d88532f44fd18e398a7e397828c7ed9
xTavares <joeltavaresxz@gmail.com>
2026-06-07 18:47:41 +0100

    Update dragging.lua

--- src/modules/dragging.lua
+++ src/modules/dragging.lua
@@ -2,17 +2,21 @@ local UserInputService = game:GetService("UserInputService")
 
 local Dragging = {}
 
-function Dragging:MakeDraggable(frame)
+function Dragging:MakeDraggable(guiObject)
 	local dragging = false
-	local dragInput = nil
-	local mouseStart = nil
-	local frameStart = nil
+	local moved = false
+	local dragInput
+	local startMouse
+	local startPosition
 
-	frame.InputBegan:Connect(function(input)
+	guiObject.Active = true
+
+	guiObject.InputBegan:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
 			dragging = true
-			mouseStart = input.Position
-			frameStart = frame.Position
+			moved = false
+			startMouse = input.Position
+			startPosition = guiObject.Position
 
 			input.Changed:Connect(function()
 				if input.UserInputState == Enum.UserInputState.End then
@@ -22,7 +26,7 @@ function Dragging:MakeDraggable(frame)
 		end
 	end)
 
-	frame.InputChanged:Connect(function(input)
+	guiObject.InputChanged:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 			dragInput = input
 		end
@@ -30,16 +34,27 @@ function Dragging:MakeDraggable(frame)
 
 	UserInputService.InputChanged:Connect(function(input)
 		if dragging and input == dragInput then
-			local delta = input.Position - mouseStart
+			local delta = input.Position - startMouse
 
-			frame.Position = UDim2.new(
-				frameStart.X.Scale,
-				frameStart.X.Offset + delta.X,
-				frameStart.Y.Scale,
-				frameStart.Y.Offset + delta.Y
+			if math.abs(delta.X) > 3 or math.abs(delta.Y) > 3 then
+				moved = true
+			end
+
+			guiObject.Position = UDim2.new(
+				startPosition.X.Scale,
+				startPosition.X.Offset + delta.X,
+				startPosition.Y.Scale,
+				startPosition.Y.Offset + delta.Y
 			)
 		end
 	end)
+
+	function guiObject:GetAttribute()
+	end
+
+	return function()
+		return moved
+	end
 end
 
 return Dragging
