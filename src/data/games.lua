f1121951df6627f3469512d2ed9aee649173043f
xTavares <joeltavaresxz@gmail.com>
2026-06-08 01:34:02 +0100

    Update games.lua

--- src/data/games.lua
+++ src/data/games.lua
@@ -3,5 +3,10 @@ return {
 		name = "Sail for Brainrots",
 		placeId = "89046742932569",
 		status = "🟢"
+	},
+	{
+		name = "Hack Vault for Brainrots",
+		placeId = "137069154816703",
+		status = "🟢"
 	}
 }
