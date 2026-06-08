bfd0f8ac52a59d9e130f77220242d04616a4e0f4
xTavares <joeltavaresxz@gmail.com>
2026-06-08 02:33:52 +0100

    Update 137069154816703.lua

--- src/games/137069154816703.lua
+++ src/games/137069154816703.lua
@@ -1,1 +1,38 @@
 -- Hack Vault for Brainrots
+-- hack vault for brainrots
+
+return function(section)
+    local elements = loadstring(game:HttpGet(getgitpath("src").."elements.lua"))()
+
+    local plr = game:GetService("Players").LocalPlayer
+    getgenv().FarmRots = false
+
+    elements:Toggle("Farm Brainrots", section, function(v)
+        if v then
+            getgenv().FarmRots = true
+
+            while getgenv().FarmRots do
+                for _, br in pairs(workspace.EntitiesFolder:GetChildren()) do
+                    plr.Character:MoveTo(Vector3.new(-2494, 4, -726))
+                    task.wait(0.5)
+                    if not br:GetAttribute("SpawnZone") == 22 then
+                        continue
+                    end
+
+                    if not br.PrimaryPart then
+                        continue
+                    end
+
+                    plr.Character:MoveTo(br.PrimaryPart.Position)
+                    task.wait()
+                    repeat fireproximityprompt(br.PrimaryPart.TakeBrainrotPrompt) task.wait() until not br.PrimaryPart or br.PrimaryPart:FindFirstChild("Attachment")
+                    plr.Character:MoveTo(Vector3.new(77, 4, -729))
+                    task.wait(1)
+                end
+                task.wait()
+            end
+        else
+            getgenv().FarmRots = false
+        end
+    end)
+end
