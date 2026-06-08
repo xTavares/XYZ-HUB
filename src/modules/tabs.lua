3e51cb87b5c5de0da863f88e0e6b894031c8aea4
xTavares <joeltavaresxz@gmail.com>
2026-06-08 02:14:26 +0100

    Update tabs.lua

--- src/modules/tabs.lua
+++ src/modules/tabs.lua
@@ -4,30 +4,30 @@ function Tabs:Create(sections, utils)
 	local controller = {}
 	controller.CurrentSection = nil
 
-	function controller:Switch(section)
-		if self.CurrentSection == section then
-			self.CurrentSection.TabBtn.BackgroundTransparency = 1
+	local function hideSection(section)
+		section.TabBtn.BackgroundTransparency = 1
+		section.Container.Visible = false
+		section.Container.Position = UDim2.new(0.5, 0, 1, 0)
+	end
 
-			utils:Tween(self.CurrentSection.Container, {
-				Position = UDim2.new(0.5, 0, 1, 0)
-			}, 0.18)
+	local function showSection(section)
+		section.Container.Visible = true
+		section.Container.Position = UDim2.new(0.5, 0, 0, 0)
+		section.TabBtn.BackgroundTransparency = 0
+	end
+
+	function controller:Switch(section)
+		for _, otherSection in pairs(sections) do
+			hideSection(otherSection)
 		end
 
-		section.Container.Visible = true
-		section.TabBtn.BackgroundTransparency = 0
-
-		utils:Tween(section.Container, {
-			Position = UDim2.new(0.5, 0, 0, 0)
-		}, 0.18)
-
+		showSection(section)
 		self.CurrentSection = section
 	end
 
 	function controller:Init(defaultSection)
 		for _, section in pairs(sections) do
-			section.TabBtn.BackgroundTransparency = 1
-			section.Container.Visible = false
-			section.Container.Position = UDim2.new(0.5, 0, 1, 0)
+			hideSection(section)
 
 			section.TabBtn.MouseButton1Click:Connect(function()
 				self:Switch(section)
